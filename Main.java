0b5a404e709bc0258bb1a5d5da64e2d6715f1975
Ezekiel <156817493+HeitorHMA@users.noreply.github.com.>
2024-11-29 15:05:25 -0300

    tst

--- Main.java
+++ Main.java
@@ -1,22 +1,9 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println(10 * 15);
-        String name = "john";
-        System.out.println(name);
+        int length = 4;
+        int width = 6;
+        int area = length * width;
+        System.out.println("the area of the rectangle is :" + area);
+}}
 
-        int myNum;
-        myNum = 15;
-        System.out.println(myNum);
-
-        final int myNumber = 192;
-        System.out.println(myNumber);
-        char myLetter = 'H';
-        System.out.println(myLetter);
-        Boolean myBool = true;
-        System.out.println(myBool);
-        float myFloatNum = 1.23f;
-        System.out.println(myFloatNum);
-    }
-}
-// Variables :
 
