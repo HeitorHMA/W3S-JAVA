f84d5437580552237408d92f666356e53edc9ca3
Ezekiel <156817493+HeitorHMA@users.noreply.github.com.>
2024-11-29 14:52:30 -0300

    Learn variables

--- Main.java
+++ Main.java
@@ -1,7 +1,22 @@
 public class Main {
     public static void main(String[] args){
         System.out.println(10 * 15);
+        String name = "john";
+        System.out.println(name);
+
+        int myNum;
+        myNum = 15;
+        System.out.println(myNum);
+
+        final int myNumber = 192;
+        System.out.println(myNumber);
+        char myLetter = 'H';
+        System.out.println(myLetter);
+        Boolean myBool = true;
+        System.out.println(myBool);
+        float myFloatNum = 1.23f;
+        System.out.println(myFloatNum);
     }
 }
-//this is a singe line comment
-/* this is a multi-line comment */
+// Variables :
+
